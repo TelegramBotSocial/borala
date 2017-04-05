3b3fffa123ce3c350f4a68bb064c01ba8d0456d5
Beto Noronha <beto@imperialweb.com.br>
2017-04-05 17:41:20 -0300

    mensagens de boas vindas e inicialização de btns

--- src/template/borala/Main.java
+++ src/template/borala/Main.java
@@ -6,6 +6,10 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramBotAdapter;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ChatAction;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendChatAction;
 import com.pengrad.telegrambot.request.SendMessage;
@@ -13,21 +17,22 @@ import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 
-import dbsql.borala.Connections;
+//import dbsql.borala.Connections;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		TelegramBot bot = TelegramBotAdapter.build("352316358:AAEPgQbbp0cMjlrTlNhbf6fYj1JhgqKRXu0");
-		Connections conexao = new Connections();
+		TelegramBot bot = TelegramBotAdapter.build("360545896:AAHfiQzjzY28klViOtAsu9Y2sFfTXdQN_Z8");
+
+		//Connections conexao = new Connections();
 
 		//objeto responsável por receber as mensagens
 		GetUpdatesResponse updatesResponse;	
 		//objeto responsável por gerenciar o envio de respostas
 		SendResponse sendResponse;
 		//objeto responsável por gerenciar o envio de ações do chat
-		BaseResponse baseResponse;
+		BaseResponse baseResponse;		
 
 		int m=0;
 
@@ -46,15 +51,24 @@ public class Main {
 				//objeto criado no projeto
 				Message msg = new Message(update.message().text());
 				String resposta = msg.getResult();
+
 				if(resposta!=null){
-					
-					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), resposta));
-					//envio de "Escrevendo" antes de enviar a resposta
 					baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
-					while(!baseResponse.isOk() || !sendResponse.isOk()){}
-				
+					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), resposta).parseMode(ParseMode.HTML));
+
+					while(!baseResponse.isOk() || !sendResponse.isOk()){
+						System.out.println("Desculpe, estamos com problema em nosso servidor.");
+					}
+					
+					Keyboard keyboard = new ReplyKeyboardMarkup(
+							new KeyboardButton[]{
+									new KeyboardButton("Minha Localização").requestLocation(true),
+									new KeyboardButton("Outra Localização").requestLocation(true)
+							}
+					);   
+					bot.execute(new SendMessage(update.message().chat().id(),"msg teste").replyMarkup(keyboard));
 				}else{
-					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "Ops... Não conseguimos entender"));
+					sendResponse = bot.execute(new SendMessage(update.message().chat().id(), "Comando <b>NÃO</b> processado por nosso sistema, tente novamente ... ").parseMode(ParseMode.HTML));
 				}
 
 			}
--- src/template/borala/Message.java
+++ src/template/borala/Message.java
@@ -8,17 +8,18 @@ public class Message {
 		if(msg!=null){
 			msg = msg.toLowerCase();
 			this.setMsgInput(msg);
-		} else{
-			
-		}
-		 
+		}		 
 	}
 	
 	public String getResult(){
-		if(this.getMsgInput().equals("/start")){
-			return "Seja bem-vindo ao Borala :)";
+		if(this != null){
+			if(this.getMsgInput()!=null){
+				return texts(this.getMsgInput());
+			}else{
+				return location(this.getMsgInput()); // location
+			}
 		}else{
-			return null;
+			return null; //msg não recebida
 		}
 	}
 
@@ -30,4 +31,18 @@ public class Message {
 		this.msgInput = msgInput;
 	}
 	
+	public String texts(String msg){
+		if(msg.equals("/start")){
+			return "Seja Bem-vindo ao <strong>Borala</strong> bot :) Para utilizar nosso bot é necessário: enviar sua <b>LOCALIZAÇÃO</b>; posteriormente navegar em nossos menu de categorias para encontrar o lugar <b>IDEAL</b> mais proximo. <code>E caso precise entrar em contato conosco, digite '/suporte'</code> !";
+		}else if(msg.equals("/suporte")){
+			return "Caso não tenhamos conseguido te <b>ajudar</b> ou tenha alguma <b>sugestão</b>, entre em contato com nosso <i>suporte</i> na nossa central de atendimentos: www.boralabot.com.br/suporte ... <strong>Até MAIS</strong>";
+		}else{
+			return "Infelizmente <b>NÃO</b> conseguimos entender. Por favor, antes de <b>qualquer</b> coisa, envie sua <pre>localização</pre> para que possamos te ajudar.";
+		}
+	}
+	
+	public String location(String loc){
+		//tratar localização
+		return "você enviou uma localização";
+	}
 }
